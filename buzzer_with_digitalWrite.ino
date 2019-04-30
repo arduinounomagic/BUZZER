60413408013f1e16c87f00cc2c55094b386f4e7e
arduinounomagic <arduinounomagic@gmail.com>
2019-04-30 19:07:45 +0530

    Update buzzer_with_digitalWrite.ino

--- buzzer_with_digitalWrite.ino
+++ buzzer_with_digitalWrite.ino
@@ -2,10 +2,11 @@
 *How to operate buzzer by Arduino uno
 *This is a very basic example which will help you to understand basic operation of buzzer using Arduino Uno.
 *
-*for more detail about this project please visit:
-*https://arduinounomagic.blogspot.com/2019/02/buzzer-with-arduino.html
+*for more detail about this project please visit:https://arduinounomagic.com/2019/02/buzzer-with-arduino.html
+*
 *Copyright (C) 2007 Free Software Foundation, Inc. <arduinounomagic@gmail.com>
-*for more projects based on Arduino uno please visit: https://arduinounomagic.blogspot.com/
+*
+*for more projects based on Arduino uno please visit: https://arduinounomagic.com/
 */
 
 int BUZZER=7; // connect buzzer at pin 7
